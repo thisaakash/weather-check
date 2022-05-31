84638a72755088000219b14b5cdf39a844d06f05
Aakash Shah <78583334+thisaakash@users.noreply.github.com>
2022-05-31 16:56:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ button.addEventListener('click',function()
         document.getElementById('image').src = `https:${icon}`;
     })
 
- fetch('https://api.countapi.xyz/hit/aakash-weather')
+ fetch('https://api.countapi.xyz/hit/aakash-weather/visits')
     .then(response => response.json())
     .then(data => {
     })
